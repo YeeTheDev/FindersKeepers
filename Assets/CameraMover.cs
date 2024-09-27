359391c7ca5af436b6a9e5e50ced30f99832677f
Yee <yee.devindie@gmail.com>
2024-09-27 00:19:54 -0600

    Camera moves with keyboard

--- Assets/CameraMover.cs
+++ Assets/CameraMover.cs
@@ -1,25 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class CameraMover : MonoBehaviour
 {
-    [SerializeField] Transform lookTarget;
-    [SerializeField] bool invertControls;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float moveToIslandSpeed;
+
+    bool moving;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(1))
+        if (moving) { return; }
+
+        if (Input.GetButton("Horizontal"))
         {
-            Vector3 rotation = Vector3.zero;
-            rotation.y = Input.GetAxis("Mouse X");
-            rotation.x = Input.GetAxis("Mouse Y");
-
-            if (invertControls) { rotation *= -1; }
-
-            lookTarget.Rotate(rotation.normalized * rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime);
         }
+        if (Input.GetButton("Vertical"))
+        {
+            transform.position += Vector3.up * Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+            {
+                StartCoroutine(MoveToIsland(hit.transform.position));
+            }
+        }
+    }
+
+    private IEnumerator MoveToIsland(Vector3 islandPosition)
+    {
+        moving = true;
+
+        float timer = 0;
+        Vector3 lastPosition = transform.position;
+
+        while (timer < moveToIslandSpeed)
+        {
+            yield return new WaitForEndOfFrame();
+
+            Vector3 lerpPoint = Vector3.Lerp(lastPosition, islandPosition, timer / moveToIslandSpeed);
+            transform.position = lerpPoint;
+
+            timer += Time.deltaTime;
+        }
+
+        transform.position = islandPosition;
+
+        moving = false;
     }
 }
